ab7dee89c23aeaf53a9061ba9d0cab911ab81596
brunosas88 <bruno.sas@live.com>
2023-01-17 10:32:40 -0300

    Adding dictionary readonly and translate methods
    Dictionary and translation methods
    will serve to manipulate the position informed
    by the player and position understood by the program

---
+++ Chess/Util/Constants.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess.Util
+{
+	public class Constants
+	{
+		public static readonly Dictionary<string, int> LineReference = new Dictionary<string, int>
+		{
+			{"8", 0}, {"7", 1}, {"6", 2}, {"5", 3}, {"4", 4}, {"3", 5}, {"2", 6}, {"1", 7}
+		};
+		public static readonly Dictionary<string, int> ColumnReference = new Dictionary<string, int>
+		{
+			{"a", 0}, {"b", 1}, {"c", 2}, {"d", 3}, {"e", 4}, {"f", 5}, {"g", 6}, {"h", 7}
+		};
+	}
+}
