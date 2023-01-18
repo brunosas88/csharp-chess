e7da38019ae5cd63d3eba05929a6b4716a4e53a8
brunosas88 <bruno.sas@live.com>
2023-01-18 17:11:08 -0300

    Adding knight move

--- Chess/Entities/Knight.cs
+++ Chess/Entities/Knight.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Chess.Entities.Enum;
+using Chess.Utils;
 
 namespace Chess.Entities
 {
@@ -25,7 +26,65 @@ namespace Chess.Entities
 
 		public List<string> Move(string currentPosition)
 		{
-			throw new NotImplementedException();
+			int[] realPosition = Util.GetRealPosition(currentPosition);
+			int currentLinePosition = realPosition[0], currentColumnPosition = realPosition[1], newLinePosition, newColumnPosition;
+			int up, left, right, down;
+			List<string> possibleMoves = new List<string>();
+
+
+
+			// posições acima 
+			newLinePosition = currentLinePosition - 2;
+			if (newLinePosition >= 0)
+			{
+				newColumnPosition = currentColumnPosition + 1;
+				if (newColumnPosition <= 7)				
+					possibleMoves.Add(Util.NominatePosition(newLinePosition, newColumnPosition));				
+
+				newColumnPosition = currentColumnPosition - 1;
+				if (newColumnPosition >= 0)
+					possibleMoves.Add(Util.NominatePosition(newLinePosition, newColumnPosition));
+			}
+
+			newLinePosition = currentLinePosition - 1;
+			if (newLinePosition >= 0)
+			{
+				newColumnPosition = currentColumnPosition + 2;
+				if (newColumnPosition <= 7)
+					possibleMoves.Add(Util.NominatePosition(newLinePosition, newColumnPosition));
+
+				newColumnPosition = currentColumnPosition - 2;
+				if (newColumnPosition >= 0)
+					possibleMoves.Add(Util.NominatePosition(newLinePosition, newColumnPosition));
+			}
+
+			// posições abaixo 
+			newLinePosition = currentLinePosition + 1;
+			if (newLinePosition <= 7)
+			{
+				newColumnPosition = currentColumnPosition + 2;
+				if (newColumnPosition <= 7)
+					possibleMoves.Add(Util.NominatePosition(newLinePosition, newColumnPosition));
+
+				newColumnPosition = currentColumnPosition - 2;
+				if (newColumnPosition >= 0)
+					possibleMoves.Add(Util.NominatePosition(newLinePosition, newColumnPosition));
+			}
+
+			newLinePosition = currentLinePosition + 2;
+			if (newLinePosition <= 7)
+			{
+				newColumnPosition = currentColumnPosition + 1;
+				if (newColumnPosition <= 7)
+					possibleMoves.Add(Util.NominatePosition(newLinePosition, newColumnPosition));
+
+				newColumnPosition = currentColumnPosition - 1;
+				if (newColumnPosition >= 0)
+					possibleMoves.Add(Util.NominatePosition(newLinePosition, newColumnPosition));
+			}
+
+
+			return possibleMoves;
 		}
 	}
 }
