7266242a6435f862819d7e05fd2b5fd7263a04ec
brunosas88 <bruno.sas@live.com>
2023-01-18 15:44:07 -0300

    Adding rook move

--- Chess/Entities/Rook.cs
+++ Chess/Entities/Rook.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Chess.Entities.Enum;
+using Chess.Utils;
 
 namespace Chess.Entities
 {
@@ -25,7 +26,39 @@ namespace Chess.Entities
 
 		public List<string> Move(string currentPosition)
 		{
-			throw new NotImplementedException();
+			int[] realPosition = Util.GetRealPosition(currentPosition);
+			int currentLinePosition = realPosition[0], currentColumnPosition = realPosition[1], newLinePosition, newColumnPosition;
+			List<string> possibleMoves = new List<string>();
+
+			newLinePosition = currentLinePosition;
+			for (int up = currentLinePosition; up > 0; up--)
+			{
+				newLinePosition -= 1;
+				possibleMoves.Add(Util.NominatePosition(newLinePosition, currentColumnPosition));				
+			}
+
+			newLinePosition = currentLinePosition;
+			for (int down = currentLinePosition; down < 7; down++)
+			{
+				newLinePosition += 1;
+				possibleMoves.Add(Util.NominatePosition(newLinePosition, currentColumnPosition));
+			}
+
+			newColumnPosition = currentColumnPosition;
+			for (int right = currentColumnPosition; right < 7; right++)
+			{
+				newColumnPosition += 1;
+				possibleMoves.Add(Util.NominatePosition(currentLinePosition, newColumnPosition));
+			}
+
+			newColumnPosition = currentColumnPosition;
+			for (int left = currentColumnPosition; left > 0; left--)
+			{
+				newColumnPosition -= 1;
+				possibleMoves.Add(Util.NominatePosition(currentLinePosition, newColumnPosition));
+			}
+
+			return possibleMoves;
 		}
 	}
 }
